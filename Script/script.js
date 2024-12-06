2823ae35915bff90e4d5c724d061b20fec0c4b54
Lucca Stern Bianchi <lucca.bianchi@aluno.ifsp.edu.br>
2024-12-06 16:45:22 -0300

    a

--- Script/script.js
+++ Script/script.js
@@ -3,6 +3,14 @@ const aside = document.querySelector('aside');
 const latido = new Audio('../Áudios/Latido.mp3');
 const logo1 = document.querySelectorAll('.logo')[0];
 const logo2 = document.querySelectorAll('.logo')[1];
+const btn1 = document.querySelector();
+const btn2 = document.querySelector();
+const btn3 = document.querySelector();
+const btn4 = document.querySelector();
+const btn5 = document.querySelector();
+const btn6 = document.querySelector();
+const btn7 = document.querySelector();
+const btn8 = document.querySelector();
 let isShown = false;
 logo1.addEventListener('mouseenter', () => {
     latido.play();
@@ -19,3 +27,10 @@ menu.addEventListener('click', () => {
         isShown = true;
     }
 });
+let precos = [61, 61, 61, 61, 30, 10, 20, 300];
+let produtos = ["Ração Pedigree Adulto", "", "", "", "Condicionador Pet Clean", "Lenço Umedecido para Gatos", "Banho Seco para Gatos", "Cama para Gatos"];
+let i;
+let total = 0;
+function AddProd (i){
+    total += produtos[i];
+}
